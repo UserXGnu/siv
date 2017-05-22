318b0e4b2aaff2f0e5d08e3cc39b06014c9040af
userx <user_x@riseup.net>
2017-05-22 16:04:07 +0100

    adding some dependencies install script

---
+++ dependencies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+OS=$(awk '/DISTRIB_ID=/' /etc/*-release | sed 's/DISTRIB_ID=//' | tr '[:upper:]' '[:lower:]')
+
+if [ -z "$OS" ]; then
+    OS=$(awk '{print $1}' /etc/*-release | tr '[:upper:]' '[:lower:]')
+fi
+
+if [ "$OS" == "arch" ]; then
+	sudo pacman -S gcc nasm make intltool pkg-config gnome-doc-utils devhelp gtk3 gstreamer clutter webkitgtk3 libgda gobject-introspection
+	echo -e "\n\n\n\t\t\t\tPLEASE, INSTALL JWASM FROM AUR REPOSITORY\n\n\n"
+elif [ "$OS" == "ubuntu" ]; then
+	sudo apt install build-essentials gcc nasm pkg-config libgtk-3-dev libgstreamer1.0-dev libclutter-1.0-dev libwebkit2gtk-4.0-dev libgda-5.0-dev libgtk-3-doc gstreamer1.0-doc libclutter-1.0-doc libwebkit2gtk-4.0-doc libgda-5.0-doc 
+	echo -e "\n\n\n\t\t\tPLEASE, INSTALL JWASM BY FOLLOWING THE jwasm_install.txt INSTRUCTIONS\n\n\n"
+fi
+
